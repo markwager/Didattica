89ff7cb308f3d06d4f399752e7643fd21f79165c
markwager <valerioceccoli@gmail.com>
2026-09-02 21:06:03 +0200

    fine cap3

---
+++ C/Cap3/breakcont_gotolabels.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+continue funziona solo in cicli come for e while, non switch.
+serve a saltare un'iterazione quando si verifica una condizione
+
+usa il goto soltanto in cicli annidati, in cui il break esce solo dal ciclo più interno
+
+*/
+
+int main(){
+
+}
